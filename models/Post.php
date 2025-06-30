8757e4ac8cb1184194a5fff82f84423e7e28f6fa
MLD-pro <morgan.ld.pro@gmail.com>
2025-06-30 13:18:56 +0000

    push 1

---
+++ models/Post.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * @author : Gaellan
+ * @link : https://github.com/Gaellan
+ */
+require_once "User.php";
+require_once "Category.php";
+
+class Post {
+    private ?int $id = null;
+    private string $title;
+    private string $excerpt;
+    private string $content;
+    private string $author;
+    private DateTime $created_at;
+
+    
+    public function __construct(string $title, string $excerpt, string $content, string $author, DateTime $created_at) {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->content = $content;
+        $this->author = $author;
+        $this->created_at = $created_at;
+    }
+
+
+
+    public function getId(): ?int 
+    {
+        return $this -> id;
+    }
+
+    public function getTitle(): string 
+    {
+        return $this -> title;
+    }
+    
+    public function getExcerpt(): string 
+    {
+        return $this -> excerpt;
+    }
+
+    
+    public function getContent(): string
+    {
+        return $this -> content;
+    }
+    
+    public function getAuthor(): string
+    {
+        return $this -> author;
+    }
+    
+    public function getCreatedAt(): DateTime
+    {
+        return $this -> created_at;                           
+    }
+
+    
+    
+    public function setId(int $id): void 
+    {
+        $this->id = $id;
+    }
+    
+     public function setTitle(string $title): void 
+    {
+        $this->title = $title;
+    }
+
+    public function setExcerpt(string $excerpt): void 
+    {
+        $this->excerpt = $excerpt;
+    }
+    
+     public function setContent(string $content): void 
+    {
+        $this->content = $content;
+    }
+
+    public function setAuthor(string $author): void 
+    {
+        $this->author = $author;
+    }
+    
+    public function setCreatedAt(DateTime $created_at): void 
+    {
+        $this->created_at = $created_at;
+    }
+}
---
+++ models/User.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @author : Gaellan
+ * @link : https://github.com/Gaellan
+ */
+
+
+class User {
+    private ?int $id = null;
+    private string $username;
+    private string $email;
+    private string $password;
+    private string $role;
+    private DateTime $created_at;
+
+    
+    public function __construct(string $username, string $email, string $password, string $role, DateTime $created_at) {
+        $this->username = $username;
+        $this->email = $email;
+        $this->setPassword($password);
+        $this->role = $role;
+        $this->created_at = $created_at;
+    }
+
+
+
+    public function getId(): ?int 
+    {
+        return $this -> id;
+    }
+
+    public function getUsername(): string 
+    {
+        return $this -> username;
+    }
+    
+    public function getEmail(): string 
+    {
+        return $this -> email;
+    }
+
+    
+    public function getPassword(): string
+    {
+        return $this -> password;
+    }
+    
+    public function getRole(): string
+    {
+        return $this -> role;
+    }
+    
+    public function getCreatedAt(): DateTime
+    {
+        return $this -> created_at;
+    }
+
+    
+    
+    public function setId(int $id): void 
+    {
+        $this->id = $id;
+    }
+    
+     public function setUsername(string $username): void 
+    {
+        $this->username = $username;
+    }
+
+    public function setEmail(string $email): void 
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword(string $password): void 
+    {
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+    }
+    
+    public function setRole(string $role): void 
+    {
+        $this->role = $role;
+    }
+    
+    public function setCreatedAt(DateTime $created_at): void 
+    {
+        $this->created_at = $created_at;
+    }
+}
